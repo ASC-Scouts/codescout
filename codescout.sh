c78ccf978e7f6f3bba118e3078f83e4c4859ae61
Vincent Fortin <vincent.fortin@ec.gc.ca>
2024-05-10 11:12:11 -0400

    lanceur bash ajoute

---
+++ codescout.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+code=$1
+./codescout.py -m "Un castor joue avec:et comme les autres" -c $code -i 2 -b 3
+./codescout.py -m "Un sourire est une:cle qui ouvre:bien des coeurs" -c $code -i 2 -b 3
+./codescout.py -m "Essayez de quitter:ce monde en le:laissant un peu:meilleur que:vous ne l'avez trouve" -c $code -i 2 -b 3
+./codescout.py -m "L'ambition de faire:le bien est la:seule qui compte" -c $code -i 2 -b 3
+./codescout.py -m "L'optimisme est:une forme de courage:qui donne confiance:aux autres" -c $code -i 2 -b 3
+./codescout.py -m "La vie est trop:courte pour:qu'on se dispute" -c $code -i 2 -b 3
+./codescout.py -m "La bonne humeur:est aussi contagieuse:que la rougeole" -c $code -i 2 -b 3
+./codescout.py -m "Les connaissances:qu'on a cherchees:restent" -c $code -i 2 -b 3
+./codescout.py -m "Y'a du boulot:pour les castors" -c $code -i 2 -b 3
+./codescout.py -m "Contentez vous de:ce que vous avez:et faites-en:le meilleur:usage possible" -c $code -i 2 -b 3
+./codescout.py -m "Le bonheur ne vient:pas a ceux qui:l'attendent assis" -c $code -i 2 -b 3
+./codescout.py -m "Sois pret:Be Prepared" -c $code -i 2 -b 3
+./codescout.py -m "Le scout merite:et fait confiance" -c $code -i 2 -b 3
+./codescout.py -m "Le scout combat:pour la justice" -c $code -i 2 -b 3
+./codescout.py -m "Le scout:partage avec tous" -c $code -i 2 -b 3
+./codescout.py -m "Le scout est:frere de tous" -c $code -i 2 -b 3
+./codescout.py -m "Le scout:protege la vie" -c $code -i 2 -b 3
+./codescout.py -m "Le scout:fait equipe" -c $code -i 2 -b 3
+./codescout.py -m "Le socut fait:tout de son mieux" -c $code -i 2 -b 3
+./codescout.py -m "Le scout:repand la joie" -c $code -i 2 -b 3
+./codescout.py -m "Le scout respecte:le travail" -c $code -i 2 -b 3
+./codescout.py -m "Le scout est:maitre de lui meme" -c $code -i 2 -b 3
