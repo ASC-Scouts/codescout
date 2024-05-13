f4de93fe376800deb92d19fffa512ba8ca78b695
Vincent Fortin <vincent.fortin@ec.gc.ca>
2024-05-13 11:38:07 -0400

    MAJ en preparation de la distribution

---
+++ tests/codescout.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+CODESCOUT=../src/codescout/codescout.py
+
+for CODE in soleil musical
+do
+    $CODESCOUT -m "Un castor joue avec:et comme les autres" -c $CODE -b 3 -s code_${CODE}.png -i 2 --legende "vincent.fortin@gmail.com"
+    $CODESCOUT -m "Un castor joue avec:et comme les autres" -c $CODE -b 3 -s code_${CODE}_decode.png --decoder
+    $CODESCOUT -m "Un castor joue avec:et comme les autres" -c $CODE -b 3 -s code_${CODE}_decode.png --decoder --legende
+done
